6f57d7505b65ff23bf753b336bd2a8bb32d2ca08
sdiehl <stephen.m.diehl@gmail.com>
2024-03-07 12:52:39 +0000

    make runner work

--- src/runner/vllm_runner.py
+++ src/runner/vllm_runner.py
@@ -7,8 +7,20 @@ def run_vllm(model_name, inputs, quantized=False, max_tokens=1024, num_beams=4):
     """
     Run the given model using vllm and return the generated text.
     """
-    model_name = "zerolink/zsql-en-postgres"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    system = "Translate English to Postgres SQL."
+    prompt = f"Generate SQL for the following question: {inputs}"
+
+    message = [
+        {"role": "system", "content": system},
+        {"role": "user", "content": prompt},
+    ]
+
+    prompt = tokenizer.apply_chat_template(
+        message, add_generation_prompt=True, tokenize=False
+    )
+
     if quantized:
         llm = LLM(
             model=model_name,
@@ -22,15 +34,18 @@ def run_vllm(model_name, inputs, quantized=False, max_tokens=1024, num_beams=4):
         )
 
     sampling_params = SamplingParams(
-        n=1,
-        best_of=num_beams,
-        use_beam_search=True,
-        stop_token_ids=[tokenizer.eos_token_id],
+        # n=1,
+        # best_of=num_beams,
+        # use_beam_search=True,
+        # stop_token_ids=[tokenizer.eos_token_id],
         max_tokens=max_tokens,
-        temperature=0,
+        temperature=0.1,
     )
 
-    outputs = llm.generate(inputs, sampling_params)
-    generated_text = outputs.outputs[0].text
-
+    outputs = llm.generate(prompt, sampling_params)
+    generated_text = outputs[0].outputs[0].text
+    print(outputs[0].outputs[0])
     return generated_text
+
+
+print(run_vllm("zerolink/zsql-en-postgres", "What are the best selling products?"))
