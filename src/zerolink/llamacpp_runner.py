4533db0fab52b5be2bb868d3a34e3630cab2540f
sdiehl <stephen.m.diehl@gmail.com>
2024-03-11 11:10:39 +0000

    download model files

--- src/zerolink/llamacpp_runner.py
+++ src/zerolink/llamacpp_runner.py
@@ -1,8 +1,10 @@
+import os
 import textwrap
+import subprocess
 from typing import Optional
 from llama_cpp import Llama
-from llama_cpp.llama_speculative import LlamaPromptLookupDecoding
 from llama_cpp.llama_types import ChatCompletionRequestMessage
+from llama_cpp.llama_speculative import LlamaPromptLookupDecoding
 
 
 class ModelRunner(object):
@@ -10,14 +12,35 @@ class ModelRunner(object):
     Model evaluation using the llama.cpp
     """
 
-    def __init__(self, model_name: str, max_tokens=1024):
+    def __init__(self, model_name: str, max_tokens=1024, quantization="q5_0"):
         self.model_name = model_name
+        self.gguf_name = f"{model_name}_{quantization}.gguf"
         self.max_tokens = max_tokens
         self.system = "Translate English to Postgres SQL."
+        self.model_folder = os.path.join(os.getcwd(), ".models")
 
     def setup(self) -> None:
+        url = f"https://huggingface.co/zerolink/{self.model_name}/resolve/main/{self.gguf_name}"
+
+        # if the model folder doesn't exist, create it
+        if not os.path.exists(self.model_folder):
+            os.makedirs(self.model_folder)
+
+        subprocess.run(
+            [
+                "curl",
+                "-L",
+                url,
+                "-o",
+                self.model_folder,
+            ]
+        )
+
+        # .models/zsql-en-postgres/zsql-en-postgres_q5_0.gguf
+        model_file = os.path.join(self.model_folder, self.gguf_name)
+
         self.model = Llama(
-            model_path=self.model_name,
+            model_path=model_file,
             n_gpu_layers=-1,
             chat_format="chatml",
             n_ctx=2048,
