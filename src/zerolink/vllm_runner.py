b44128eac28602ff9369e7744580fc7052cf646d
sdiehl <stephen.m.diehl@gmail.com>
2024-03-11 14:06:15 +0000

    autodetect inference framework

--- src/zerolink/vllm_runner.py
+++ src/zerolink/vllm_runner.py
@@ -27,6 +27,7 @@ class ModelRunner(object):
         self.model = LLM(
             model=self.model_name,
             tensor_parallel_size=torch.cuda.device_count(),
+            log_stats=False,
         )
         self.params = SamplingParams(
             n=1,
--- src/zerolink/wrapper.py
+++ src/zerolink/wrapper.py
@@ -1,24 +1,37 @@
+import logging
 from typing import Optional
+from importlib.util import find_spec
 
 
 def to_sql(
     question: str,
     context: Optional[str] = None,
     model_name: str = "zerolink/zsql-en-postgres",
-    framework: Optional[str] = "mlx",
+    framework: Optional[str] = None,
 ) -> str:
     """
     Generate SQL from a question and context.
     """
 
-    if framework == "mlx":
-        from .mlx_runner import ModelRunner
-    elif framework == "vllm":
-        from .vllm_runner import ModelRunner
-    elif framework == "llamacpp":
-        from .llamacpp_runner import ModelRunner
+    if not framework:
+        if find_spec("mlx") is not None:
+            logging.info("Using MLX")
+            from .mlx_runner import ModelRunner
+        elif find_spec("vllm") is not None:
+            logging.info("Using VLLM")
+            from .vllm_runner import ModelRunner
+        elif find_spec("llamacpp") is not None:
+            logging.info("Using Llama.cpp")
+            from .llamacpp_runner import ModelRunner
     else:
-        raise ValueError(f"Framework {framework} not supported")
+        if framework == "mlx":
+            from .mlx_runner import ModelRunner
+        elif framework == "vllm":
+            from .vllm_runner import ModelRunner
+        elif framework == "llamacpp":
+            from .llamacpp_runner import ModelRunner
+        else:
+            raise ValueError(f"Framework {framework} not supported")
 
     model = ModelRunner(model_name)
     model.setup()
